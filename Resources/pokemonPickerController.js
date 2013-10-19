1421ee3ff9e15d933577fe08d22b2d5f07504e35
xanth- <bananasdoom@gmail.com>
2013-10-19 21:01:00 +1000

    some changes

--- Resources/pokemonPickerController.js
+++ Resources/pokemonPickerController.js
@@ -1,65 +1,71 @@
 // Continue To battle screen
-pickerDoneButton.addEventListener('click', function(e){
-	switchWin(pokemonBattle, "Battle");
+pickerDoneButton.addEventListener('click', function (e) {
+    switchWin(pokemonBattle, "Battle");
 });
 // return to main menu
-pickerBackButton.addEventListener('click', function(e){
-	switchWin(mainMenuView);
+pickerBackButton.addEventListener('click', function (e) {
+    switchWin(mainMenuView);
 });
 
 // Page Carosel animation
-pageCarousel2.addEventListener('touchstart', function(e){
-	var touchPos = {x: e.x, y: e.y};
-	lastTouchPosition = e.source.convertPointToView(touchPos, pokemonPicker);
+pageCarousel2.addEventListener('touchstart', function (e) {
+    var touchPos = {
+        x: e.x,
+        y: e.y
+    };
+    lastTouchPosition = e.source.convertPointToView(touchPos, pokemonPicker);
 });
 
-pageCarousel2.addEventListener('touchmove', function(e){
-	var touchPos = {x: e.x, y: e.y};
-	var newTouchPosition = e.source.convertPointToView(touchPos, pokemonPicker);
+pageCarousel2.addEventListener('touchmove', function (e) {
+    var touchPos = {
+        x: e.x,
+        y: e.y
+    };
+    var newTouchPosition = e.source.convertPointToView(touchPos, pokemonPicker);
 
-	// Only support moving pages horizontally
-	pageCarousel2.left += newTouchPosition.x - lastTouchPosition.x;
-	
-	lastTouchPosition = newTouchPosition;
+    // Only support moving pages horizontally
+    pageCarousel2.left += newTouchPosition.x - lastTouchPosition.x;
+
+    lastTouchPosition = newTouchPosition;
 });
 
-pageCarousel2.addEventListener('touchend', function(e){
-	// Determine which view is most in the centre of the screen
-	for (var i = 0; i < pages.length; i++){
-		
-		var left = pageCarousel2.left + pages[i].left;
-		var right = pageCarousel2.left + pages[i].left + pages[i].width;
-		var centre = platformWidth / 2;
-		
-		if (left <= centre && right >= centre){
-			// Move this page to the centre
-			var newLeft = (platformWidth - pages[i].width) / 2;
-			
-			// How much do we need to move all of the pages by?
-			var deltaX = newLeft - (pageCarousel2.left + pages[i].left);
-			
-			//pageCarousel2.left += deltaX;
-			
-			var endLeft = pageCarousel2.left + deltaX;
-			
-			var animation = Titanium.UI.createAnimation({
-				left: endLeft,
-				curve: Titanium.UI.ANIMATION_CURVE_EASE_IN_OUT
-			});
-			
-			animation.addEventListener('complete', function(e){
-				pageCarousel2.left = endLeft;
-			});
-			
-			pageCarousel2.animate(animation);
-			break;
-		}
-	}
+pageCarousel2.addEventListener('touchend', function (e) {
+    // Determine which view is most in the centre of the screen
+    for (var i = 0; i < pages.length; i++) {
+
+        var left = pageCarousel2.left + pages[i].left;
+        var right = pageCarousel2.left + pages[i].left + pages[i].width;
+        var centre = platformWidth / 2;
+
+        if (left <= centre && right >= centre) {
+            // Move this page to the centre
+            var newLeft = (platformWidth - pages[i].width) / 2;
+
+            // How much do we need to move all of the pages by?
+            var deltaX = newLeft - (pageCarousel2.left + pages[i].left);
+
+            //pageCarousel2.left += deltaX;
+
+            var endLeft = pageCarousel2.left + deltaX;
+
+            var animation = Titanium.UI.createAnimation({
+                left: endLeft,
+                curve: Titanium.UI.ANIMATION_CURVE_EASE_IN_OUT
+            });
+
+            animation.addEventListener('complete', function (e) {
+                pageCarousel2.left = endLeft;
+            });
+
+            pageCarousel2.animate(animation);
+            break;
+        }
+    }
 });
 // Add event listensers to the pages
-page1.addEventListener('dblclick', function(e){
-	switchWin(mainMenuView);
+page1.addEventListener('dblclick', function (e) {
+    switchWin(mainMenuView);
 });
-page2.addEventListener('dblclick', function(e){
-	switchWin(mainMenuView);
+page2.addEventListener('dblclick', function (e) {
+    switchWin(mainMenuView);
 });

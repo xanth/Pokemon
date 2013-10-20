db11a5fd7d387a0609217bcac6132323d932b06d
Adam--Miller <adam_miller@windowslive.com>
2013-10-21 00:32:58 +1000

    Got rid of dodgey variable for videoURL. Messing with variables to try get video.

--- Resources/introVideoView.js
+++ Resources/introVideoView.js
@@ -1,18 +1,14 @@
-var introVideo = Ti.UI.createWindow({
+	var introVideo = Ti.UI.createWindow({
 	title: 'Intro Video',
-	backgroundColor: '#000000',
 	orientationModes: (Ti.UI.PORTRAIT, Ti.UI.LANDSCAPE_LEFT, Ti.UI.LANDSCAPE_RIGHT)
 }); 
 
-//A fix to play the video I found on StackOverflow
-var videoURL = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'video/introVid.mp4');
-
 //creates the video player and plays the video 
 var videoPlayer = Ti.Media.createVideoPlayer({
-	url: videoURL.nativePath,
-	backgroundColor: "blue",
+	url: (Ti.Filesystem.resourcesDirectory, 'video/introVid.mp4'),
+	backgroundColor: '#000',
 	movieControlMode: Ti.Media.VIDEO_CONTROL_NONE,
-	scalingMode: Ti.Media.VIDEO_SCALING_ASPECT_FILL,
+	scalingMode: Ti.Media.VIDEO_SCALING_ASPECT_FIT,
 	fullscreen: true,
 	autoplay: true
 });
